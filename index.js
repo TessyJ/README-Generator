cccba31430ecb87ec9123860b57b3bacdf9f7df0
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-04-09 19:59:11 -0400

    Installed inquirer nodule

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        message:'What\'s the title of your porject',
+        name:'title'
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log('hello')
+    inquirer.prompt(questions);
+     //generateMarkdown()
 }
 
 // Function call to initialize app
