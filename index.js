4461cd5caf9b5aeee4184c69dedcb2c64c8d8e49
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-04-09 20:24:55 -0400

    Added Description questions

--- index.js
+++ index.js
@@ -5,11 +5,36 @@ const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type:'input',
-        message:'What\'s the title of your project',
-        name:'title'
-    }
+  {
+    type: "input",
+    message: "What's the title of your project",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please provide the project description",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please provide an installation instructions for this project",
+    name: "installaton",
+  },
+  {
+    type: "input",
+    message: "Please provide an usage information for this project",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Please provide the contribution guidelines for this project",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Please provide the test instructions for this project",
+    name: "test",
+  },
 ];
 
 
@@ -24,8 +49,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((answers) =>{
         writeToFile(answers.title+'.md',answers)
-    })
-     //generateMarkdown()
+    }) 
 }
 
 // Function call to initialize app
